46a32dc170fb70cdfada9d83e7531a21873ddcf1
pesutak <peter@sutak.net>
2026-01-14 20:32:06 +0000

    f 1

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const WHAT = process.env.WHAT || 'Hello World';
 
 console.log(`Starting service - will log "${WHAT}" every ${INTERVAL}ms`);
 // devel
+// feature 1
 
 setInterval(() => {
   console.log(WHAT);

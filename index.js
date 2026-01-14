d005f812acc4025c6cf7ac83df17ddc5f0339d64
pesutak <peter@sutak.net>
2026-01-14 19:52:07 +0000

    devel

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const INTERVAL = parseInt(process.env.INTERVAL || '5000', 10);
 const WHAT = process.env.WHAT || 'Hello World';
 
 console.log(`Starting service - will log "${WHAT}" every ${INTERVAL}ms`);
+// devel
 
 setInterval(() => {
   console.log(WHAT);
